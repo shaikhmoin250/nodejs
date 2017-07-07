b2345aeea3fcfeba09e9e621c650a940da3554c4
Moin Shaikh <shaikhmoin250@gmail.com>
2017-07-07 22:15:49 +0530

    It shows the data when it is connected to server

---
+++ callback.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+
+fs.readdir('.', function (err, files) {
+  if (err) {
+    console.log('Error finding files: ' + err)
+  } else {
+    console.log('files: ' + files);
+    files.forEach(function (filename, fileIndex) {
+      fs.readFile(filename, function (err, buf) {
+        if (err) {
+          console.log('Error reading file:' + err);
+        } else {
+          console.log(buf.toString());
+        }
+      });
+    });
+  }
+});
